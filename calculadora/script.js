54fc3254e13aaf3fe09bc1824fdddd8e335201aa
LeonardoPov <leonardopoved@gmail.com>
2023-08-21 11:52:13 -0500

    first commit

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,50 +1,42 @@
-
 // Obtener referencia al campo de entrada y al historial de cálculos
 const inputField = document.calculator.ans;
 const historyElement = document.querySelector('#calculation-history');
 
 // Inicializar variables
-let calculationHistory = [];
+let calculationHistory = []; 
 let resultShown = false;
 let lastClearTime = 0;
 
 
+// Actualiza el historial de cálculos en el elemento HTML
 function updateHistory() {
-    historyElement.innerHTML = calculationHistory.reverse().join('<br>');
-    calculationHistory.reverse();
+    historyElement.innerHTML = calculationHistory.map(item => {
+        const [expression, result] = item.split('=').map(str => str.trim());
+        return `<div class="calculation-entry">${expression}<br>= ${result}</div>`;
+    }).reverse().join('');
     historyElement.scrollTop = 0;
 }
 
 
+// Limpia el campo de entrada
 function clearInput() {
     inputField.value = '';
 }
 
+// Limpia todo el historial de cálculos
 function clearHistory() {
     calculationHistory = [];
     updateHistory();
 }
 
-
+// Valida el contenido del campo de entrada
 function validateInput() {
     const expression = inputField.value;
-    
-    if (/[\+\-\*\/]{2,}/.test(expression)) {
-        showInputError("Error");
-        return false;
-    }
 
-    if (/^[\*\/]/.test(expression)) {
-        showInputError("Error");
-        return false;
-    }
-
-    if (/^0\d/.test(expression)) {
-        showInputError("Error");
-        return false;
-    }
-
-    if (/[\+\-\*\/]$/.test(expression)) {
+    if (/[\+\-\*\/]{2,}/.test(expression) ||
+        /^[\*\/]/.test(expression) ||
+        /^0\d/.test(expression) ||
+        /[\+\-\*\/]$/.test(expression)) {
         showInputError("Error");
         return false;
     }
@@ -52,38 +44,41 @@ function validateInput() {
     return true;
 }
 
-
+// Muestra un mensaje de error en el campo de entrada
 function showInputError(message) {
     inputField.value = message;
     inputField.classList.add('error');
     setTimeout(() => {
         inputField.value = '';
         inputField.classList.remove('error');
-    }, 1000); // Limpia el mensaje después de 2 segundos
+    }, 1000); // Limpia el mensaje después de 1 segundo
 }
 
-
+// Realiza el cálculo y actualiza el historial
 function calculate() {
     if (validateInput()) {
         const expression = inputField.value;
+
         try {
             const result = eval(expression);
-            calculationHistory.unshift(`${expression} = ${result}`);
+            calculationHistory.push(`${expression} = ${result}`);
             updateHistory();
             inputField.value = result;
             resultShown = true;
+            hidePossibleResult();
         } catch (error) {
-            showError('Error en el cálculo.');
             console.error('Error in calculation:', error);
+            inputField.value = 'Error en el cálculo.';
         }
     }
 }
 
 
+// Limpia el campo de entrada o el historial de cálculos
 function clearInputOrHistory() {
     const currentTime = new Date().getTime();
     if (currentTime - lastClearTime < 500) {
-        if (inputField.value === '') {
+        if (inputField.value === '' || resultShown) {
             clearHistory();
         } else {
             clearInput();
@@ -94,9 +89,11 @@ function clearInputOrHistory() {
     lastClearTime = currentTime;
 }
 
-
-
-
+// Limpia el campo de entrada y el resultado posible
+function clearInputAndResult() {
+    inputField.value = '';
+    hidePossibleResult();
+}
 
 // Agregar detectores de eventos a los botones
 const buttons = document.querySelectorAll('input[type="button"]');
@@ -108,7 +105,7 @@ buttons.forEach(button => {
                 if (inputField.value === '' || resultShown) {
                     clearHistory();
                 } else {
-                    clearInputOrHistory();
+                    clearInputAndResult(); // Cambio aquí
                 }
             } else {
                 const functionName = window[value.toLowerCase()];
@@ -127,6 +124,54 @@ buttons.forEach(button => {
                 resultShown = false;
             }
             inputField.value += value;
+            
+            // Calcula el posible resultado y muéstralo
+            const expression = inputField.value;
+            try {
+                const possibleResult = eval(expression);
+                showPossibleResult(possibleResult);
+            } catch (error) {
+                // Manejo de error si es necesario
+            }
         });
+    }
+});
+
+
+function showPossibleResult(possibleResult) {
+    const resultElement = document.getElementById('possible-result');
+    resultElement.textContent = `= ${possibleResult}`;
+    resultElement.style.display = 'block';
+}
+
+function hidePossibleResult() {
+    const resultElement = document.getElementById('possible-result');
+    resultElement.textContent = '';
+    resultElement.style.display = 'none';
+}
+
+
+// Obtener referencia al botón de borrado dígito por dígito
+const backspaceButton = document.getElementById('backspace-button');
+
+// Agregar el evento de clic al botón de borrado dígito por dígito
+backspaceButton.addEventListener('click', function() {
+    if (resultShown) {
+        clearInputAndResult();
+    } else {
+        const currentValue = inputField.value;
+        inputField.value = currentValue.slice(0, -1); // Elimina el último carácter
+
+        const newExpression = inputField.value;
+        if (newExpression === '') {
+            hidePossibleResult();
+        } else {
+            try {
+                const possibleResult = eval(newExpression);
+                showPossibleResult(possibleResult);
+            } catch (error) {
+                hidePossibleResult();
+            }
+        }
     }
 });
